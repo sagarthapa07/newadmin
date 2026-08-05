756f9d4327f6ba4292a9347374318c0e6e5cee4f
Sagar <sagarthapa7757@gmail.com>
2026-08-05 16:19:24 +0530

    new change ee

--- src/app/core/Services/common.ts
+++ src/app/core/Services/common.ts
@@ -1,169 +1,149 @@
-import { Inject, Injectable } from '@angular/core';
-import { CookieService } from 'ngx-cookie-service';
-import * as CryptoJS from 'crypto-js';
-import { environment } from '../../../environments/environment';
-import * as moment from 'moment';
-import 'moment-timezone';
+  import { Inject, Injectable } from '@angular/core';
+  import { CookieService } from 'ngx-cookie-service';
+  import * as CryptoJS from 'crypto-js';
+  import { environment } from '../../../environments/environment';
+  import * as moment from 'moment';
+  import 'moment-timezone';
 
-@Injectable({
-  providedIn: 'root',
-})
-export class Common {
-  constructor(@Inject(CookieService) private Cookie: CookieService) {}
+  @Injectable({
+    providedIn: 'root',
+  })
+  export class Common {
+    constructor(@Inject(CookieService) private Cookie: CookieService) {}
 
-  // public setCookie(key: string, value: string, expireTime: any): boolean {
-  //   this.Cookie.set(key, value, {
-  //     expires: expireTime,
-  //     path: '/',
-  //     sameSite: 'Lax',
-  //     domain: environment.domain,
-  //   });
+    // public setCookie(key: string, value: string, expireTime: any): boolean {
+    //   this.Cookie.set(key, value, {
+    //     expires: expireTime,
+    //     path: '/',
+    //     sameSite: 'Lax',
+    //     domain: environment.domain,
+    //   });
 
-  //   return true;
-  // }
+    //   return true;
+    // }
 
-  // public getCookie(key: string) {
-  //   return this.Cookie.get(key);
-  // }
+    // public getCookie(key: string) {
+    //   return this.Cookie.get(key);
+    // }
 
-  public setCookie(key: string, value: string, expireTime: any): boolean {
-    console.log('============== setCookie ==============');
+    public setCookie(key: string, value: string, expireTime: any): boolean {
+      console.log('============== setCookie ==============');
+      console.log('Key :', key);
+      console.log('Value :', value);
+      console.log('Expire :', expireTime);
+      console.log('Current Host :', window.location.hostname);
+      console.log('Current Origin :', window.location.origin);
+      this.Cookie.set(key, value, {
+        expires: expireTime,
+        path: '/',
+        sameSite: 'Lax',
+        domain: environment.domain,
+        // CHANGE :
+        // Secure add kiya hai.
+        // HTTPS domain pe cookie secure honi chahiye.
+        secure: window.location.protocol === 'https:',
+      });
+      console.log('Cookie After Save :', this.Cookie.get(key));
+      console.log('All Cookies :', document.cookie);
+      return true;
+    }
 
-    console.log('Key :', key);
+    public getCookie(key: string) {
+      const cookie = this.Cookie.get(key);
+      console.log('Reading Cookie');
+      console.log('Key :', key);
+      console.log('Value :', cookie);
+      return cookie;
+    }
 
-    console.log('Value :', value);
+    public deleteCookie(key: string) {
+      document.cookie = encodeURIComponent(key) + '=; expires=Thu, 01-Jan-1970 00:00:01 GMT; path=/';
+    }
 
-    console.log('Expire :', expireTime);
-
-    console.log('Environment Domain :', environment.domain);
-
-    console.log('Current Host :', window.location.hostname);
-
-    console.log('Current Origin :', window.location.origin);
-
-    this.Cookie.set(key, value, {
-      expires: expireTime,
-
-      path: '/',
-
-      sameSite: 'Lax',
-
-      domain: environment.domain,
-
-      // CHANGE :
-      // Secure add kiya hai.
-      // HTTPS domain pe cookie secure honi chahiye.
-      secure: window.location.protocol === 'https:',
-    });
-
-    console.log('Cookie After Save :', this.Cookie.get(key));
-
-    console.log('All Cookies :', document.cookie);
-
-    return true;
-  }
-
-  public getCookie(key: string) {
-    const cookie = this.Cookie.get(key);
-
-    console.log('Reading Cookie');
-
-    console.log('Key :', key);
-
-    console.log('Value :', cookie);
-
-    return cookie;
-  }
-
-  public deleteCookie(key: string) {
-    document.cookie = encodeURIComponent(key) + '=; expires=Thu, 01-Jan-1970 00:00:01 GMT; path=/';
-  }
-
-  public deleteAllCookie() {
-    var cookies = document.cookie.split('; ');
-    for (var c = 0; c < cookies.length; c++) {
-      var d = window.location.hostname.split('.');
-      while (d.length > 0) {
-        var cookieBase =
-          encodeURIComponent(cookies[c].split(';')[0].split('=')[0]) +
-          '=; expires=Thu, 01-Jan-1970 00:00:01 GMT; domain=' +
-          d.join('.') +
-          ' ;path=';
-        var p = location.pathname.split('/');
-        document.cookie = cookieBase + '/';
-        while (p.length > 0) {
-          document.cookie = cookieBase + p.join('/');
-          p.pop();
+    public deleteAllCookie() {
+      var cookies = document.cookie.split('; ');
+      for (var c = 0; c < cookies.length; c++) {
+        var d = window.location.hostname.split('.');
+        while (d.length > 0) {
+          var cookieBase =
+            encodeURIComponent(cookies[c].split(';')[0].split('=')[0]) +
+            '=; expires=Thu, 01-Jan-1970 00:00:01 GMT; domain=' +
+            d.join('.') +
+            ' ;path=';
+          var p = location.pathname.split('/');
+          document.cookie = cookieBase + '/';
+          while (p.length > 0) {
+            document.cookie = cookieBase + p.join('/');
+            p.pop();
+          }
+          d.shift();
         }
-        d.shift();
       }
     }
-  }
 
-  decrypt(key: string, value: string): string {
-    const bytes = CryptoJS.AES.decrypt(value, key);
-    return bytes.toString(CryptoJS.enc.Utf8);
-  }
+    decrypt(key: string, value: string): string {
+      const bytes = CryptoJS.AES.decrypt(value, key);
+      return bytes.toString(CryptoJS.enc.Utf8);
+    }
 
-  encryptData(data: string) {
-    console.log('Cookie Key :', environment.cookieKey);
-    console.log('Data Before Encrypt :', data);
-    try {
-      const key = CryptoJS.enc.Utf8.parse(environment.cookieKey);
-      const iv = CryptoJS.enc.Utf8.parse('');
-      const encrypted: any = CryptoJS.AES.encrypt(data, key, {
-        keySize: 128 / 8,
-        iv: iv,
-        mode: CryptoJS.mode.CBC,
-        padding: CryptoJS.pad.Pkcs7,
-      });
-      return encrypted.toString();
-    } catch (e) {
-      console.error(e);
-      return null;
+    encryptData(data: string) {
+      console.log('Data Before Encrypt :', data);
+      try {
+        const key = CryptoJS.enc.Utf8.parse(environment.cookieKey);
+        const iv = CryptoJS.enc.Utf8.parse('');
+        const encrypted: any = CryptoJS.AES.encrypt(data, key, {
+          keySize: 128 / 8,
+          iv: iv,
+          mode: CryptoJS.mode.CBC,
+          padding: CryptoJS.pad.Pkcs7,
+        });
+        return encrypted.toString();
+      } catch (e) {
+        console.error(e);
+        return null;
+      }
+    }
+
+    encryptData1(data: string) {
+      try {
+        const key = CryptoJS.enc.Utf8.parse(environment.cookieKey);
+        const iv = CryptoJS.enc.Utf8.parse('');
+        const encrypted: any = CryptoJS.AES.encrypt(data, key, {
+          keySize: 128 / 8,
+          iv: iv,
+          mode: CryptoJS.mode.CBC,
+          padding: CryptoJS.pad.Pkcs7,
+        });
+
+        return encrypted.toString().replace(/=/g, '|').replace(/\//g, '!').replace(/\+/g, '-');
+      } catch (e) {
+        console.error(e);
+        return null;
+      }
+    }
+    decryptData(encryptedText: string) {
+      encryptedText = encryptedText.replace(/\|/g, '=').replace(/!/g, '/').replace(/\-/g, '+');
+      try {
+        const key = CryptoJS.enc.Utf8.parse(environment.cookieKey);
+        const iv = CryptoJS.enc.Utf8.parse('');
+        const decrypted = CryptoJS.AES.decrypt(encryptedText.trim(), key, {
+          keySize: 128 / 8,
+          iv: iv,
+          mode: CryptoJS.mode.CBC,
+          padding: CryptoJS.pad.Pkcs7,
+        });
+        return decrypted.toString(CryptoJS.enc.Utf8);
+      } catch (e) {
+        console.error(e);
+        return null;
+      }
+    }
+
+    getCurrentDate() {
+      return moment.tz('America/New_York').format('YYYY');
+    }
+
+    getCurrentYear() {
+      return moment.tz('America/New_York').format('YYYY');
     }
   }
-
-  encryptData1(data: string) {
-    try {
-      const key = CryptoJS.enc.Utf8.parse(environment.cookieKey);
-      const iv = CryptoJS.enc.Utf8.parse('');
-      const encrypted: any = CryptoJS.AES.encrypt(data, key, {
-        keySize: 128 / 8,
-        iv: iv,
-        mode: CryptoJS.mode.CBC,
-        padding: CryptoJS.pad.Pkcs7,
-      });
-
-      return encrypted.toString().replace(/=/g, '|').replace(/\//g, '!').replace(/\+/g, '-');
-    } catch (e) {
-      console.error(e);
-      return null;
-    }
-  }
-  decryptData(encryptedText: string) {
-    encryptedText = encryptedText.replace(/\|/g, '=').replace(/!/g, '/').replace(/\-/g, '+');
-    try {
-      const key = CryptoJS.enc.Utf8.parse(environment.cookieKey);
-      const iv = CryptoJS.enc.Utf8.parse('');
-      const decrypted = CryptoJS.AES.decrypt(encryptedText.trim(), key, {
-        keySize: 128 / 8,
-        iv: iv,
-        mode: CryptoJS.mode.CBC,
-        padding: CryptoJS.pad.Pkcs7,
-      });
-      return decrypted.toString(CryptoJS.enc.Utf8);
-    } catch (e) {
-      console.error(e);
-      return null;
-    }
-  }
-
-  getCurrentDate() {
-    return moment.tz('America/New_York').format('YYYY');
-  }
-
-  getCurrentYear() {
-    return moment.tz('America/New_York').format('YYYY');
-  }
-}
